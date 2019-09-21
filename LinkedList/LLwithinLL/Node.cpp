9b4685701401b457f39f339ee61b9844cc56b765
YogeshJain96 <54388005+YogeshJain96@users.noreply.github.com>
2019-09-21 23:07:25 +0530

    Update Node.cpp

--- LinkedList/LLwithinLL/Node.cpp
+++ LinkedList/LLwithinLL/Node.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+template<class T>
+class Node
+{
+	private:
+		T data;
+		Node* next;
 
+	public:
+		T getData()
+		{
+			return data;
+		}
+		T setData(T ele)
+		{
+			data=ele;
+
+		}
+		Node* getNext()
+		{
+			return next;
+		}
+		Node* setNext(Node* temp)
+		{
+			next=temp;
+		}
+	};
